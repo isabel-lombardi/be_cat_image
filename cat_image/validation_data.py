ff42fd83087e7a55e0de67e0b8fbba951886d786
Giuseppe Marcucci <marcucci.dev@gmail.com>
2020-12-28 11:21:17 +0100

    [FEAT] Validation structure close #10

---
+++ cat_image/validation_data.py
@@ -0,0 +1,16 @@
+class ValidationData:
+    @staticmethod
+    def is_username_valid(login_data: dict):
+        return True
+
+    @staticmethod
+    def is_password_valid(login_data: dict):
+        return True
+
+    @staticmethod
+    def is_photos_number_valid():
+        return True
+
+    @staticmethod
+    def is_photos_format_valid():
+        pass
