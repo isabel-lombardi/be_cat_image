b3bb25c9ede105c4553716b0168e6b5bedb46c5a
David92p <davide.panetta@hotmail.com>
2020-12-30 20:51:21 +0100

    [FEAT]Validate ID or E-Mail  close #11

--- cat_image/validation_data.py
+++ cat_image/validation_data.py
@@ -1,7 +1,14 @@
+from re import search
 class ValidationData:
     @staticmethod
     def is_username_valid(login_data: dict):
-        return True
+        regex = '^[a-z0-9A-Z]+[\._]?[a-z0-9A-Z]+[@]\w+[.]\w{2,3}$'
+        if search(regex, login_data["username"]):
+            return True
+        elif len(login_data["username"]) >= 4 or len(login_data["username"]) <= 10:
+            return True
+        else:
+            return False
 
     @staticmethod
     def is_password_valid(login_data: dict):
