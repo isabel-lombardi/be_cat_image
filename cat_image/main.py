f78606023b28519bbb2d0e5849d2842e76b80115
Giuseppe Marcucci <marcucci.dev@gmail.com>
2020-12-31 14:42:13 +0100

    [FEAT] update and output close #8

--- cat_image/main.py
+++ cat_image/main.py
@@ -3,6 +3,8 @@ from flask import Flask, request
 # from flask_cors import CORS
 
 from cat_image.validation_data import ValidationData
+from cat_image.categorization.processing_images import ProcessingImages
+from cat_image.categorization.categorization import Categorization
 
 app = Flask(__name__)  # create the Flask app
 '''CORS(app, origins="http://localhost:8080", allow_headers=[
@@ -25,6 +27,59 @@ def login():
         return reply_message, 400
     else:
         return {'status': 'OK'}, 200
+
+
+# Route for handling the upload page logic
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'GET':
+        return {'status': 'error', 'message': 'GET Method Not Allowed'}, 405
+
+    if request.method == 'POST':
+        reply_message = {}
+        images_data = request.get_json()
+        if not ValidationData.is_images_number_valid(images_data):
+            reply_message['status'] = 'error'
+            reply_message['message'] = 'Incorrect images quantity'
+            return reply_message, 400
+
+        processing_images = ProcessingImages(images_data)
+        try:
+            decoded_images = processing_images.decoding()  # decode images and return in a list
+        except:
+            reply_message['status'] = 'error'
+            reply_message['message'] = 'Decoding failed'
+            return reply_message, 500
+
+        try:
+            wrong_images_id = ValidationData.images_format_validation(decoded_images)
+        except:
+            reply_message['status'] = 'error'
+            reply_message['message'] = 'images_format_validation failed'
+            return reply_message, 500
+
+        if len(wrong_images_id) > 0:
+            reply_message['status'] = 'error'
+            reply_message['message'] = 'Incorrect images format'
+            reply_message['wrong_images_id'] = wrong_images_id
+            return reply_message, 400
+        else:
+            try:
+                categorization = Categorization()
+                categorization.load_input_images()  # read images from dir and put in self.images list
+                result = categorization.use_template()  # analyze self.images list and return results
+            except:
+                reply_message['status'] = 'error'
+                reply_message['message'] = 'Categorization failed'
+                return reply_message, 500
+
+            reply_message['status'] = 'OK'
+            reply_message['message'] = 'Categorization successfully processed'
+            reply_message['result'] = result
+            try:
+                ProcessingImages.empty_folder()
+            except:
+                return reply_message, 200
 
 
 if __name__ == "__main__":
