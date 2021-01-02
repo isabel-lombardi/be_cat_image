45078749afada2ff3db396aaac96e00550e926d1
Giuseppe Marcucci <marcucci.dev@gmail.com>
2021-01-02 10:54:15 +0100

    [TEST] Test Flask with class Validation close #29

---
+++ tests/test_route_login.py
@@ -0,0 +1,59 @@
+from flask import json
+
+from cat_image.main import app
+
+
+def test_route_login():
+    response = app.test_client().post(
+        '/login',
+        data=json.dumps({'username': 'admin', 'password': 'admin'}),
+        content_type='application/json',
+    )
+
+    data = json.loads(response.get_data(as_text=True))
+
+    assert response.status_code == 200
+    assert data['status'] == "OK"
+    print(response)
+
+
+def test_route_login_bad_username():
+    response = app.test_client().post(
+        '/login',
+        data=json.dumps({'username': 'ab', 'password': 'admin'}),
+        content_type='application/json',
+    )
+
+    data = json.loads(response.get_data(as_text=True))
+
+    assert response.status_code == 400
+    assert data['status'] == "error"
+    assert data['message'] == 'Incorrect username'
+
+
+def test_route_login_bad_password():
+    response = app.test_client().post(
+        '/login',
+        data=json.dumps({'username': 'admin', 'password': 'abc'}),
+        content_type='application/json',
+    )
+
+    data = json.loads(response.get_data(as_text=True))
+
+    assert response.status_code == 400
+    assert data['status'] == "error"
+    assert data['message'] == 'Incorrect password'
+
+
+'''def test_route_login_get():
+    response = app.test_client().get(
+        '/login',
+        data=json.dumps({'username': 'admin', 'password': 'admin'}),
+        content_type='application/json',
+    )
+
+    data = json.loads(response.get_data(as_text=True))
+
+    assert response.status_code == 405
+    assert data['status'] == "error"
+    print(response)'''
